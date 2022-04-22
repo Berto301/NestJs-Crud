3a83e89102e492eb2ed6cd26da4f6b708e578b00
Berto <ratsimandresyhajatiana@gmail.com>
2022-04-22 16:48:22 +0300

    Setup mongodb

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,42 @@
 import { Injectable } from "@nestjs/common";
+import { AuthDto } from "./dto";
 
+import * as argon from 'argon2'
+import { PrismaService } from "src/prisma/prisma.service";
 @Injectable({})
 export class AuthService{
+   constructor(private prisma: PrismaService){
+
+   }
    login(){
+    
     return {msg: "Login"}
    }
-   signup(){
-    return {msg: "Signin"}
+  async signup(dto:AuthDto){
+     const {password,email} = dto
+         //generate password hash
+    const hash = await argon.hash(password);
+    //save the new user
+    let user = {}
+    console.log("lala2",dto)
+    try {
+       user = await this.prisma.user.create({
+         data: {
+           email,
+           hash,
+         },
+         select:{
+            id:true,
+            email:true,
+            lastName:true,
+            firstName:true
+         }
+       });
+    } catch (error) {
+      console.log(error) 
+    } 
+  
+    //return the user saved
+    return user
    }
 }
